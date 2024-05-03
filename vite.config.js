6107b2e9e18823355f5b85e1a9c699c5870664bd
Md. Samiul Islam <samiul.primary@gmail.com>
2024-05-03 16:56:55 +0600

    new

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   base : "/dashboard/",
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
